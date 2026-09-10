ee9e7031a477196fe5d03d46e9239803f2dbd167
lirunquan2002-png <lirunquan2002@gmail.com>
2026-09-10 14:49:15 +0800

    路径参数练习

--- project2/project2.py
+++ project2/project2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,path
+from fastapi import FastAPI,Path
 
 app = FastAPI()
 
@@ -8,5 +8,11 @@ async def root():
 
 #访问fastapi，响应：msg：你好Fastapi
 @app.get("/user/{id}")
-async def get_user(id: str,name:str):
-    return {"id": f"{id}" , "name": f"{name}"}
+async def get_user(id: int=Path(...,gt=0,lt=101,description="书籍id，取值范围1-100")):
+    return {"id": f"{id}" ,"tltle":"这是第{id}本书"}
+
+
+#需求：查找书籍的作者，路径参数name，长度范围2-10
+@app.get("/booker/{name}")
+async def get_booker(name:str = Path(...,min_length=2,max_length=4)):
+    return {"msg":f"这是{name}的信息"}
